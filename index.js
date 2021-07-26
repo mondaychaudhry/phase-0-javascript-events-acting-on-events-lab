53f193c51a96693bef45764af101fcd6ff848b6b
Monday <sobaata@gmail.com>
2021-07-26 01:15:26 -0400

    Completed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function moveDodgerLeft () {
 function moveDodgerRight () {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left= parseInt(leftNumbers, 10);
-    if (left < 360) {
+    if (left < 400) {
         dodger.style.left = `${left+1}px`
     }
 }
